fd140cacb198e7d88b83882eeee849bd92734961
milkyborbon <milkyborbon30@gmail.com>
2023-03-01 00:56:40 +0800

    update-index

--- push.js
+++ push.js
@@ -1,11 +1,13 @@
 var push  = require('web-push');
 
 let vapidKeys = {
-    publicKey: 'BGlFCGsO5eAj_kMeDjKzwpQ_ElWna-HQcb063UpT-xx8UJ1ILsOGLbwsioIRsd1JNTHmKpE5RXNKeFTQtzj0ipg',
-    privateKey: 'EdAv-CHNKmSPa0IveEDkQ-vbE-h6TEVpgcF03pYZBig'
+    publicKey: 'BG94pQ6ht1Uc3aBFoSGOed7kGfNzIRTzf5NLj87_eCiox2evWekMvGE2AvQ3yq749TOHV2CpZBmcfyQGG8kiuT0',
+    privateKey: 'NOpYZ9VCJQK_sm8oTbdKjZptsP17CQtaqygnoziw3dE'
   };
+
 
 push.setVapidDetails('mailto:milkyborbon30@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/e3jYJY0i6Cc:APA91bGEUY2SKxObTNvbWhC7fEh62JSaW5eluH6P4gtZj0HKL2zqWNOxiUIPpQ190hUae_-cYYVlzb5yBFv_OYBQZj56o7aqZQM7E-cv6PSZVtAhf9Soj0ZdN9qW8l854fF7NOGlvcXq","expirationTime":null,"keys":{"p256dh":"BBRsTZWc0mC8ARPBlShul9bk4ZgYzL9bF5fPT3nCfKFl3weLLAVFa7I4f91SXNbeDsfhq6YLRe-ar5fKf9AqSkM","auth":"0SpjNa5g7OOcQ0bLpk4_JA"}};
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/coTcJTPZYy8:APA91bF30TSMa9qGGOAZV59ATFIYrhNIX5spbJ50ToqKzTehRQ8oxN-EmaTBRVRyNxGII2tWhnEfUjljFR_Qbnampw7bE2exYLr_1RXcvJXklt6ClmB4V8t4QqAYL5eBHgbEjaGunbjb","expirationTime":null,"keys":
+{"p256dh":"BOhOjq9v2ZmxWButy280aCdRutydrG_s1oKNKOWmNXeu__5lNIR6Z9XeOITa--CNuWzSWVzTv9ib2607SLKdRa8","auth":"VDkYg DQYtLn3NbtRXWYpyQ"}};
 push.sendNotification(sub, 'This is the Test Message');
